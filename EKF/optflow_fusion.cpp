18b33d9a08ae3b3d7a32e4335175353ba9145246
Nico van Duijn <nico@auterion.com>
2019-09-04 14:39:30 +0200

    Optical Flow: use local z if below dist sensor min

--- EKF/optflow_fusion.cpp
+++ EKF/optflow_fusion.cpp
@@ -85,7 +85,7 @@ void Ekf::fuseOptFlow()
 	Vector3f vel_body = earth_to_body * vel_rel_earth;
 
 	// height above ground of the IMU
-	float heightAboveGndEst = _terrain_vpos - _state.pos(2);
+	float heightAboveGndEst = _state.pos(2) > gndclearance ? _terrain_vpos - _state.pos(2) : _state.pos(2);
 
 	// calculate the sensor position relative to the IMU in earth frame
 	Vector3f pos_offset_earth = _R_to_earth * pos_offset_body;
